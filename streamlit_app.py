44da9b74049890f3bf4f798c2b1564c444cd496d
devrana2575 <devrana2575@gmail.com>
2026-05-21 00:14:26 +0530

    Improved form layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ with col2:
 
     with col_a:
         extra = st.selectbox(
-            "Extracurricular",
+            "Extracurricular Acitivities",
             ["Yes", "No"],
         )
     with col_b:
